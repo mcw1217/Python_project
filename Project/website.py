5232cea36c7e83a158a0a48429f99d9d3af18e17
mcw1217 <mcw1217@naver.com>
2022-11-11 01:19:34 +0900

    beta test

--- Project/website.py
+++ Project/website.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov  7 15:13:38 2022
-
-@author: 김민서
-"""
-
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
@@ -13,53 +6,57 @@ def make_model():
     model = pickle.load(open("model.t","rb"))
     print(model)
     return model
-    
-def do_predict(model, age, , w2, h2):
-    tool = pickle.load(open("tool.t","rb"))
-    model = make_model()
-
-    x = [[w1,h1,w2,h2]]
-    x = tool.transform(x).toarray()
-    age = [[age]]
-    x = np.hstack([age,x])
-    
-    y_pre = model.predict(x)
-    return y_pre
-
-
-
-
-model = make_model()
-
-#============================================================
 
 webserver = Flask(__name__)
 
 @webserver.route("/")
 def index():
-    msg="welcome!"
+    msg= render_template("main.html")
     return msg
 
-@webserver.route("/survey")
-def iris():
+@webserver.route("/survey.html")
+def survey():
     msg = render_template("survey.html")
     return msg
 
 @webserver.route("/survey_ans", methods=["POST"])
-def iris_ans():
-    w1 = float(request.form["w1"])
-    h1 = float(request.form["h1"])
-    w2 = float(request.form["w2"])
-    h2 = float(request.form["h2"])
-    
-    print(w1, h1, w2, h2)
-    
-    y_pre = do_predict(model, w1, h1, w2, h2)
-    print(y_pre)
-    
-    msg = f"{w1} {w2} {h1} {h2} => {y_pre[0]}"
+def survey_ans():
+    model = make_model()   
+    tool = pickle.load(open("tool.t","rb"))
+
+    gender= request.form["gender"]
+    age= request.form["age"]
+    a = request.form["a"]
+    b = request.form["b"]
+    c = request.form["c"]
+    d = request.form["d"]
+    e = request.form["e"]
+    f = request.form["f"]
+    g = request.form["g"]
+    h = request.form["h"]
+    i = request.form["i"]
+    j = request.form["j"]
+    k = request.form["k"]
+    l = request.form["l"]
+    m = request.form["m"]
+    n = request.form["n"]
+
+    print(gender,a,b,c,d,e,f,g,h,i,j,k,l,m,n)
+    x = [[gender,a,b,c,d,e,f,g,h,i,j,k,l,m,n]]
+    x = tool.transform(x).toarray()
+    age = [[age]]
+    x = np.hstack([age,x])
+
+    y_pre = model.predict(x)
+
+    if y_pre[0] == "Positive":
+        msg = "[당뇨병 초기 검진 결과] =====> 당신은 '당뇨병' 초기 증상이 의심됩니다! 빠른 시일내에 병원에서 검사해보시기바랍니다."
+    else:
+        msg = "[당뇨병 초기 검진 결과] =====> 당신은 '당뇨병' 초기 증상이 의심되지 않습니다! 오늘도 건강한 하루되시길 바랍니다!"
     
     return msg
+
+    
 
 
 webserver.run(port = 2022)
